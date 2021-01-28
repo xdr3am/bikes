5b7088a05ff64962db72da02798d077378c9bafe
xdr3am <xdreamroyale@yandex.ru>
2021-01-29 01:39:49 +0300

    Добавляет главный блок страницы

---
+++ source/js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const navLink = document.querySelectorAll('.main-nav__link');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  })
+};
