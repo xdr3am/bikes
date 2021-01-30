1301e19bcba78c2e0e39035af2f9cedb45cc4962
xdr3am <xdreamroyale@yandex.ru>
2021-01-30 20:40:57 +0300

    Добавляет все блоки и делает ПП всей страницы

--- source/js/main.js
+++ source/js/main.js
@@ -22,3 +22,18 @@ for (let i = 0; i < navLink.length; i++) {
     navMain.classList.remove('main-nav--opened');
   })
 };
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
