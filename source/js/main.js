037a2c4d3b5b4898782c175097f6a6c72abe7afa
xdr3am <xdreamroyale@yandex.ru>
2021-02-03 20:11:24 +0300

    Исправляет ошибки eslint

--- source/js/main.js
+++ source/js/main.js
@@ -1,39 +1,40 @@
 'use strict';
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
-const navLink = document.querySelectorAll('.main-nav__link');
 
-navMain.classList.remove('main-nav--nojs');
+const navMain = document.querySelector(`.main-nav`);
+const navToggle = document.querySelector(`.main-nav__toggle`);
+const navLink = document.querySelectorAll(`.main-nav__link`);
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+navMain.classList.remove(`main-nav--nojs`);
+
+navToggle.addEventListener(`click`, function () {
+  if (navMain.classList.contains(`main-nav--closed`)) {
+    navMain.classList.remove(`main-nav--closed`);
+    navMain.classList.add(`main-nav--opened`);
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add(`main-nav--closed`);
+    navMain.classList.remove(`main-nav--opened`);
   }
 });
 
 for (let i = 0; i < navLink.length; i++) {
-  navLink[i].addEventListener('click', function (evt) {
+  navLink[i].addEventListener(`click`, function (evt) {
     evt.preventDefault();
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  })
-};
+    navMain.classList.add(`main-nav--closed`);
+    navMain.classList.remove(`main-nav--opened`);
+  });
+}
 
-const anchors = document.querySelectorAll('a[href*="#"]');
+const anchors = document.querySelectorAll(`a[href*="#"]`);
 
 for (let anchor of anchors) {
-  anchor.addEventListener("click", function (e) {
+  anchor.addEventListener(`click`, function (e) {
     e.preventDefault();
 
-    const blockID = anchor.getAttribute("href").substr(1);
+    const blockID = anchor.getAttribute(`href`).substr(1);
 
     document.getElementById(blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
+      behavior: `smooth`,
+      block: `start`,
     });
   });
 }
