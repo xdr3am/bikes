6d0d1415c05a5544d5503b7eda21d0d9ba6023b3
xdr3am <xdreamroyale@yandex.ru>
2021-02-09 18:13:58 +0300

    Исправляет отображение страницы при включении мобильного меню

--- build/js/main.js
+++ build/js/main.js
@@ -4,6 +4,7 @@
   const navMain = document.querySelector(`.main-nav`);
   const navToggle = document.querySelector(`.main-nav__toggle`);
   const navLink = document.querySelectorAll(`.main-nav__link`);
+  const page = document.querySelector(`body`);
 
   navMain.classList.remove(`main-nav--nojs`);
 
@@ -11,9 +12,11 @@
     if (navMain.classList.contains(`main-nav--closed`)) {
       navMain.classList.remove(`main-nav--closed`);
       navMain.classList.add(`main-nav--opened`);
+      page.classList.add(`main-nav--show`);
     } else {
       navMain.classList.add(`main-nav--closed`);
       navMain.classList.remove(`main-nav--opened`);
+      page.classList.remove(`main-nav--show`);
     }
   });
 
